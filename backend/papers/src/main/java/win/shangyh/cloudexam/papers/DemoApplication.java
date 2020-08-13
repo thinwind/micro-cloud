2c8c5a9b462a475dbbb5c8852e351db8ceb11214
Shang <niceshang@icloud.com>
2020-08-13 12:52:45 +0800

    添加papers服务

---
+++ backend/papers/src/main/java/win/shangyh/cloudexam/papers/DemoApplication.java
@@ -0,0 +1,13 @@
+package win.shangyh.cloudexam.papers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
