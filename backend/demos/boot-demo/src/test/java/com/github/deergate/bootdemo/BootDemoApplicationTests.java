c9f97564ffae07c6b7cb78548610d8a2395e499c
Shang <niceshang@icloud.com>
2021-04-18 13:54:10 +0800

    add demo

---
+++ backend/demos/boot-demo/src/test/java/com/github/deergate/bootdemo/BootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.deergate.bootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
